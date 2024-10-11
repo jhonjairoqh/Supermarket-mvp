392c38d92c9b6c9a931f6b74d37b7374f04169e7
jhonjairoqh <jhonjairo12345qh@gmail.com>
2024-10-11 16:46:09 -0500

    update presenter y repository

--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78a95fbe9eaa87e22ff2dcef1089809bfb19bd5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2fd8c46d76cf20f011ae39bde4fbe7a09cc5850")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
